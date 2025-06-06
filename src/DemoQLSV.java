38e058b16d00140f16fee1a9d9b933e614e5d61c
Tran Thanh Duy <thanhduy04022004@gmail.com>
2025-06-06 20:33:07 +0700

    hihi

--- src/DemoQLSV.java
+++ src/DemoQLSV.java
@@ -2,6 +2,53 @@ import java.util.Scanner;
 
 public class DemoQLSV {
     public static void main(String[] args) {
+//
+//        /*  CÁCH GIẢI 1*/
+//        //tạo ra danh sách sinh viên rỗng
+//        List<SinhVien> listSinhVien = new ArrayList<SinhVien>();
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Số lượng sinh viên muốn nhập: ");
+//        int soLuong = Integer.parseInt(sc.nextLine());
+//        for(int i = 0; i < soLuong; i++) {
+//            SinhVien sinhVien = new SinhVien();
+//
+//            System.out.println("Nhập tên sinh vien: ");
+//            sinhVien.setTenSV(sc.nextLine());
+//
+//            System.out.println("Nhập mã sinh viên: ");
+//            sinhVien.setMaSV(sc.nextLine());
+//
+//            System.out.println("Nhập điểm môn toán: ");
+//            sinhVien.setToan(Double.parseDouble(sc.nextLine()));
+//
+//            System.out.println("Nhập điểm môn lý: ");
+//            sinhVien.setLy(Double.parseDouble(sc.nextLine()));
+//
+//            System.out.println("Nhập điểm môn hóa: ");
+//            sinhVien.setHoa(Double.parseDouble(sc.nextLine()));
+//
+//            listSinhVien.add(sinhVien);
+//        }
+//        //foreach: Tự đọng duyệt hết mảng
+//        //cú pháp: for(Biến_đại_diện_cho_phần_tử_duyệt qua : mảng_Cần_duyệt)
+//        //cách 1
+//        for(SinhVien item : listSinhVien){
+//            System.out.println("Tên sinh viên: "+item.getTenSV());
+//            double dtb = ((item.getToan()) + item.getLy() + item.getHoa()) / 3;
+//            System.out.println("Điểm trung bình: "+dtb);
+//        }
+//
+//        //cách 2 dùng foreach
+////        listSinhVien.forEach(item->{
+////            System.out.println("Tên sinh viên: "+item.getTenSV());
+////            double dtb = ((item.getToan()) + item.getLy() + item.getHoa()) / 3;
+////            System.out.println("Điểm trung bình: "+dtb);
+////        });
+//
+
+        /* CÁCH GIẢI 2*/
+        List<SinhVien> listSinhVien = new ArrayList<SinhVien>();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Nhập tên của bạn: ");
