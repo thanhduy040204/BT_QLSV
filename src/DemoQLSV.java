997672c94c4cf2b00695e96013e7da5e00f1ec72
Tran Thanh Duy <thanhduy04022004@gmail.com>
2025-06-09 18:16:25 +0700

    Bai tap OOP

--- src/DemoQLSV.java
+++ src/DemoQLSV.java
@@ -1,74 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DemoQLSV {
     public static void main(String[] args) {
-//
-//        /*  CÁCH GIẢI 1*/
-//        //tạo ra danh sách sinh viên rỗng
-//        List<SinhVien> listSinhVien = new ArrayList<SinhVien>();
-//
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Số lượng sinh viên muốn nhập: ");
-//        int soLuong = Integer.parseInt(sc.nextLine());
-//        for(int i = 0; i < soLuong; i++) {
-//            SinhVien sinhVien = new SinhVien();
-//
-//            System.out.println("Nhập tên sinh vien: ");
-//            sinhVien.setTenSV(sc.nextLine());
-//
-//            System.out.println("Nhập mã sinh viên: ");
-//            sinhVien.setMaSV(sc.nextLine());
-//
-//            System.out.println("Nhập điểm môn toán: ");
-//            sinhVien.setToan(Double.parseDouble(sc.nextLine()));
-//
-//            System.out.println("Nhập điểm môn lý: ");
-//            sinhVien.setLy(Double.parseDouble(sc.nextLine()));
-//
-//            System.out.println("Nhập điểm môn hóa: ");
-//            sinhVien.setHoa(Double.parseDouble(sc.nextLine()));
-//
-//            listSinhVien.add(sinhVien);
-//        }
-//        //foreach: Tự đọng duyệt hết mảng
-//        //cú pháp: for(Biến_đại_diện_cho_phần_tử_duyệt qua : mảng_Cần_duyệt)
-//        //cách 1
-//        for(SinhVien item : listSinhVien){
-//            System.out.println("Tên sinh viên: "+item.getTenSV());
-//            double dtb = ((item.getToan()) + item.getLy() + item.getHoa()) / 3;
-//            System.out.println("Điểm trung bình: "+dtb);
-//        }
-//
-//        //cách 2 dùng foreach
-////        listSinhVien.forEach(item->{
-////            System.out.println("Tên sinh viên: "+item.getTenSV());
-////            double dtb = ((item.getToan()) + item.getLy() + item.getHoa()) / 3;
-////            System.out.println("Điểm trung bình: "+dtb);
-////        });
-//
-
-        /* CÁCH GIẢI 2*/
-        List<SinhVien> listSinhVien = new ArrayList<SinhVien>();
+        // tạo 1 danh sách sinh viên rỗng
+        List<SinhVien> danhSachSinhVien = new ArrayList<SinhVien>();
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Nhập tên của bạn: ");
-        String name = sc.nextLine();
-
-        System.out.println("Nhập mã số sinh viên: ");
-        String id = sc.nextLine();
-
-        System.out.println("Nhập điểm môn toán: ");
-        double toan = Double.parseDouble(sc.nextLine());
-
-        System.out.println("Nhập điểm môn lý: ");
-        double ly = Double.parseDouble(sc.nextLine());
-
-        System.out.println("Nhập điểm môn hóa: ");
-        double hoa = Double.parseDouble(sc.nextLine());
-
-        SinhVien sv = new SinhVien(name,id,toan,ly,hoa);
-        System.out.println("-----KẾT QUẢ-----");
-        sv.hienThiThongTin();
-
+        //cho người dùng nhập số lượng sinh viên
+        System.out.println("Số lượng sinh viên cần nhập: ");
+        int soLuong = Integer.parseInt(sc.nextLine());
+        // vòng for sẽ lặp cho đến khi đủ số lượng sinh viên người dùng cần
+        for(int i = 0; i < soLuong; i++) {
+            SinhVien sv = new SinhVien();
+            sv.nhapThongTinSinhVien();
+            danhSachSinhVien.add(sv);
+        }
+        //dùng forEach để lặp điểm trung bình và xếp loại của từng sinh viên
+        danhSachSinhVien.forEach(item ->{
+            item.tinhDiemTrungBinh();
+            item.inThongTinVaXepLoai();
+        });
+        //Sinh viên có ĐTB lớn nhất
+        SinhVien svMax = SinhVien.timKiemSinhVienCoDiemTrungBinhCaoNhat(danhSachSinhVien);
+        System.out.println("==> Sinh viên có đểm trung bình cao nhất là: "+svMax.getTenSV()+
+                " - MSSV: "+svMax.getMaSV()+" - Điểm trung bình là: "+svMax.tinhDiemTrungBinh());
+        //Danh sách sinh viên xếp loại yếu
+        SinhVien.danhSachSinhVienXepLoaiYeu(danhSachSinhVien);
+        //tra cứu tên sinh viên
+        System.out.println("Nhập tên sinh viên cần tìm: ");
+        String tenCanTim = sc.nextLine();
+        SinhVien.timSinhVienTheoTen(danhSachSinhVien, tenCanTim);
+        //tra cứu MSSV
+        System.out.println("Nhập mã sinh viên cần tìm: ");
+        String maCanTim = sc.nextLine();
+        SinhVien.timMaSinhVien(danhSachSinhVien, maCanTim);
+        // Xóa sinh viên theo yêu cầu
+        System.out.println("Nhập mã số sinh viên cần xóa: ");
+        String maCanXoa = sc.nextLine();
+        SinhVien.xoaSinhVienTheoMa(danhSachSinhVien, maCanXoa);
     }
 }
--- src/SinhVien.java
+++ src/SinhVien.java
@@ -1,46 +1,164 @@
+import java.util.List;
+import java.util.Scanner;
 
 public class SinhVien {
-    String name, Id;
-    double toan,ly,hoa;
+    //tạo các thuộc tính
+    private String tenSV;
+    private String maSV;
+    private double toan;
+    private double ly;
+    private double hoa;
+    //hàm nhập thông tin sinh viên
+    public void nhapThongTinSinhVien(){
+        Scanner sc = new Scanner(System.in);
 
-    public SinhVien(String name, String Id,double toan,double ly,double hoa) {
-        this.name = name;
-        this.Id = Id;
-        this.toan = toan;
-        this.ly = ly;
-        this.hoa = hoa;
+        System.out.println("Nhập tên sinh viên: ");
+        this.tenSV = sc.nextLine();
 
+        System.out.println("Nhập mã số sinh viên: ");
+        this.maSV = sc.nextLine();
+
+        System.out.println("Nhập điểm toán: ");
+        this.toan = Double.parseDouble(sc.nextLine());
+
+        System.out.println("Nhập điểm lý: ");
+        this.ly = Double.parseDouble(sc.nextLine());
+
+        System.out.println("Nhập điểm hoá: ");
+        this.hoa = Double.parseDouble(sc.nextLine());
     }
-
-
-
-
+    // hàm tính ĐTB
     public double tinhDiemTrungBinh(){
-        return (toan + ly + hoa) / 3;
+        return (toan+ly+hoa)/3;
     }
-
-    public String xepLoai(){
+    // hàm in thông tin điểm và xếp loại
+    public void inThongTinVaXepLoai(){
         double dtb = tinhDiemTrungBinh();
-        if(dtb >= 9){
-            return "Xuất sắc";
-        }else if (dtb >= 8 && dtb < 9){
-            return "Giỏi";
-        }else if (dtb >= 7 && dtb < 8){
-            return "Khá";
-        } else if (dtb >= 6  && dtb < 7) {
-            return "Trung Bình";
+        System.out.println("Tên sinh viên: "+this.tenSV+
+                " - MSSV: "+this.maSV+
+                " - ĐTB: "+dtb);
+        if(dtb >= 9 ){
+            System.out.println("Xếp loại 'Xuất sắc'");
+        } else if (dtb >=8 && dtb <9) {
+            System.out.println("Xếp loại 'Giỏi'");
+        } else if (dtb >= 7 && dtb < 8) {
+            System.out.println("Xếp loại 'Khá'");
+        }else if (dtb >= 6 && dtb < 7) {
+            System.out.println("Xếp loại 'Tiên tiến'");
+        }else if (dtb >= 5 && dtb < 6) {
+            System.out.println("Xếp loại 'Trung bình'");
         }else {
-            return "Yếu";
+            System.out.println("Xếp loại 'Yếu'");
+        }
+
+    }
+    // hàm tìm kiếm sinh viên DTB cao nhất trong danh sách
+    public static SinhVien timKiemSinhVienCoDiemTrungBinhCaoNhat(List<SinhVien> danhSach){
+        SinhVien svMax = danhSach.get(0);
+        double diemMax = svMax.tinhDiemTrungBinh();
+        for(int i = 1; i < danhSach.size(); i++){
+            double dtb = danhSach.get(i).tinhDiemTrungBinh();
+            if(dtb >= diemMax){
+                diemMax = dtb;
+                svMax = danhSach.get(i);
+            }
+        }
+        return svMax;
+    }
+    // hàm xử lý liệt kê các sinh viên yêú
+    public static void danhSachSinhVienXepLoaiYeu(List<SinhVien> xepLoaiYeu){
+        System.out.println("-----Danh Sách Sinh Viên Xếp Loại Yếu-----");
+        for(SinhVien  sv : xepLoaiYeu){
+            if(sv.tinhDiemTrungBinh() < 5){
+                System.out.println("Tên sinh viên: "+sv.getTenSV()+
+                        " - MSSV: "+sv.getMaSV()+" - ĐTB: "+sv.tinhDiemTrungBinh());
+            }
+        }
+    }
+    // hàm xử lý tìm kiếm theo tên
+    public static void timSinhVienTheoTen(List<SinhVien> danhSach, String tenCanTim){
+        System.out.println("-----Kết quả tìm kiếm theo tên: " + tenCanTim + "-----");
+        boolean timThay = false;
+        for(SinhVien sv : danhSach){
+            if(sv.getTenSV().equalsIgnoreCase(tenCanTim)){
+                timThay = true;
+                System.out.println("Tên sinh viên: "+sv.getTenSV()+
+                        " - MSSV: "+sv.getMaSV()+" - ĐTB: "+sv.tinhDiemTrungBinh());
+            }
+        }
+        if(!timThay){
+            System.out.println("Không tìm thấy tên sinh viên nào có tên: "+tenCanTim);
         }
     }
 
-    public void hienThiThongTin(){
-        System.out.println("Tên của bạn là: "+name);
-        System.out.println("Mã số sinh viên là: "+Id);
-        System.out.println("Điểm môn toán: "+toan);
-        System.out.println("Điểm môn lý: "+ly);
-        System.out.println("Điểm môn hóa: "+hoa);
-        System.out.println("Điểm trung bình: "+tinhDiemTrungBinh());
-        System.out.println("Xếp loại: "+xepLoai());
+    // hàm xử lý tìm kiếm theo mã
+    public static void timMaSinhVien(List<SinhVien> danhSach, String maSinhVienCanTim){
+        System.out.println("-----Kết quả tìm kiếm theo MSSV: "+maSinhVienCanTim + "-----");
+        boolean timThay = false;
+        for(SinhVien  sv : danhSach){
+            if(sv.getMaSV().equalsIgnoreCase(maSinhVienCanTim)){
+                timThay = true;
+                System.out.println("Tên sinh viên: "+sv.getTenSV()+
+                        " - MSSV: "+sv.getMaSV()+" - ĐTB: "+sv.tinhDiemTrungBinh());
+                return;
+            }
+        }
+        System.out.println("Không tìm thấy sinh viên nào có mã số: "+maSinhVienCanTim);
+    }
+    // hàm xử lý xóa theo mã
+    public static void xoaSinhVienTheoMa(List<SinhVien> danhSach, String maCanXoa){
+        boolean daxoa = false;
+        for(int i = 0; i < danhSach.size(); i++){
+            if(danhSach.get(i).getMaSV().equalsIgnoreCase(maCanXoa)){
+                daxoa = true;
+                System.out.println(">> Đã xóa sinh vien có MSSV: "+maCanXoa);
+                break;
+            }
+        }
+        if(!daxoa){
+            System.out.println(">>Không tìm thấy sinh viên nào có MSSV: "+maCanXoa);
+        }
+
+    }
+
+
+    public String getTenSV() {
+        return tenSV;
+    }
+
+    public void setTenSV(String tenSV) {
+        this.tenSV = tenSV;
+    }
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public double getToan() {
+        return toan;
+    }
+
+    public void setToan(double toan) {
+        this.toan = toan;
+    }
+
+    public double getLy() {
+        return ly;
+    }
+
+    public void setLy(double ly) {
+        this.ly = ly;
+    }
+
+    public double getHoa() {
+        return hoa;
+    }
+
+    public void setHoa(double hoa) {
+        this.hoa = hoa;
     }
 }
